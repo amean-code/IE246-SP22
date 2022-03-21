39818385da3c0b279987e9e457043db3beff3ad8
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-22 00:14:21 +0300

    More Matrix Operation Example

--- Excercies/MatrixOperation.java
+++ Excercies/MatrixOperation.java
@@ -4,9 +4,9 @@ public class MatrixOperation {
 
     public static void main(String[] args) {
         
-        int[][] zero5Matrix = zeros(5,5);
-        printMatrix(zero5Matrix);
-
+        // int[][] zero5Matrix = zeros(5,5);
+        // printMatrix(zero5Matrix);
+        question2();
     }
 
     public static int[][] zeros(int row,int col){ // row = 5 - col = 5
@@ -49,15 +49,47 @@ public class MatrixOperation {
         ]
         */
         // rows
-        for(int[] row : matrix){ // row =  [12,9,0],
+        for(int[] row : matrix){ // row =  [2,1,4],
             // items
-            for(int item : row){ // item = 1
+            for(int item : row){ // item = 2
+
+                if(item == 3 || item == 4){
+                    return;
+                }
                 // print
                 System.out.print(item+"\t");
             }
             // Next Line
             System.out.println();
 
+        }
+
+    }
+
+    public static void question2(){
+
+        // Define double array of arrays
+        double[][] matrix = {
+            {34,53,5,3.76,1},
+            {4.1,53,5,3.76,6},
+            {34,3,5,3.76,5},
+        };
+        // iterate rows
+        for(int i = 0 ; i < matrix.length ; i++){ // i = 0
+
+            // iterate columns
+            for(int j = 0 ; j < matrix[0].length ; j++){// j = 2
+
+                // check the condition
+                if( !((matrix[i][j] >= 3 && matrix[i][j] <4) || (matrix[i][j] >= 5 && matrix[i][j] <=6)) ){
+                    // if true - print
+                    System.out.print(matrix[i][j]+" ");
+                }
+
+            }
+
+            // Next Line
+            System.out.println();
         }
 
     }
