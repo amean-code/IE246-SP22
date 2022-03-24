6125910f718d50ff680ea05187a3934872ee6d9b
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-24 15:00:02 +0300

    Quiz2 Solution

--- Excercies/MinimumFinder.java
+++ Excercies/MinimumFinder.java
@@ -29,26 +29,32 @@ public class MinimumFinder {
         // loop until your nth minimum
         for (int count = 0;count<t; count++){ // count = 0
 
-            minValue = Integer.MAX_VALUE;
-            minIndex = 0;
+           
 
-            // find minimum and its index
-            for (int i=0 ; i < values.length ; i++){ // i = 0
-
-                // if this is the min value and not pinned
-                if (values[i]<minValue && !pin[i]){
-
-                    // get the value
-                    minValue=values[i];
-                    // get the index
-                    minIndex=i;
-                }
-            }
-
-            pin[minIndex] = true;
+            pin[Simay(values, pin)] = true;
         }
 
         System.out.println(t+"th minimum number is: "+minValue);
         
+    }
+
+    public static int Simay(int[] values,boolean[] pin){
+        int minValue = Integer.MIN_VALUE;
+        int minIndex = 0;
+
+        // find minimum and its index
+        for (int i=0 ; i < values.length ; i++){ // i = 0
+
+            // if this is the min value and not pinned
+            if (values[i]>minValue && !pin[i]){
+
+                // get the value
+                minValue=values[i];
+                // get the index
+                minIndex=i;
+            }
+        }
+
+        return minIndex;
     }
 }
